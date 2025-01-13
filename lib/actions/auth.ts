5b8c9fd15d056b778d9c947b332094412e725f59
DennyAge <DenX055@gmail.com>
2025-01-13 11:34:28 +0200

    add upstash

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -5,12 +5,20 @@ import {db } from "@/database/drizzle";
 import {users} from "@/database/schema";
 import {hash} from "bcryptjs";
 import { signIn } from "@/auth";
+import {headers} from "next/headers";
+import ratelimit from "@/lib/ratelimit";
+import {redirect} from "next/navigation";
 
 export const singInWithCredentials = async (
     params: Pick<AuthCredentials, "email" | "password">
 ) => {
 
     const {email, password} = params;
+
+    const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+    const {success} = await ratelimit.limit(ip)
+
+    if(!success) return redirect("/too-fast")
 
     try {
       const result = await signIn('credentials', {
@@ -31,6 +39,11 @@ export const singInWithCredentials = async (
 
 export const signUp = async (params: AuthCredentials) => {
     const {fullName, email, password, universityId, universityCard} = params
+
+    const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+    const {success} = await ratelimit.limit(ip)
+
+    if(!success) return redirect("/too-fast")
 
     const existingUser = await db
         .select()
